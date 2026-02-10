14ac16f0fc1b3d75222f7977b0eed327d597dc1d
MariaLuisaSG <mlsg.eng24@uea.edu.br>
2026-02-10 12:30:07 -0400

    Random forest model create

--- modelo_baseline/main.py
+++ modelo_baseline/main.py
@@ -1,6 +1,7 @@
 from src.data.load_data import load_data
 from src.models import decision_tree
 from src.models import logistic_regression
+from src.models import random_forest
 from src.models.train import train, evaluate
 from src.evaluation.metrics import plot_confusion_matrix
 from sklearn.model_selection import train_test_split
@@ -21,7 +22,8 @@ def main():
     #metrics = evaluate(model, X_test, y_test)
     models ={
         "Decision Tree": decision_tree.create_model(),
-        "Logistic Regression":logistic_regression.create_model()
+        "Logistic Regression":logistic_regression.create_model(),
+        "Random forest": random_forest.create_model(),
     }
     resultados = {}
     for name, model in models.items():
